c3d0dc502c56004169a7d8aa48cd82cbb542b8de
Mark Melczer <melczer7@gmail.com>
2020-05-25 17:05:54 +0200

    Nice.

--- main.c
+++ main.c
@@ -4,6 +4,29 @@
 
 #include "cursor.h"
 
+void save_to_file(Cursor *c, char *table) {
+	char *path; 
+	char *filename;
+	//printf("Please type the name of the file:\n");
+	//scanf("%s", filename); 
+	strcpy(filename, "test");
+	strcpy(path, "data/");
+	strcat(filename, ".dat");
+	strcat(path, filename);
+
+	FILE *fp;
+	fp = fopen(path, "w");
+	if (fp == NULL) {
+		printf("Couldn't open file!");
+		return;
+	}
+	//fwrite(c, sizeof(Cursor), 1, fp);
+	fputs(table, fp);
+	fputs("\nThis is a test\n", fp);
+	fclose(fp);
+	printf("Saved data to: %s\n", path);
+}
+
 
 void main(void) {
 	//printf("How many lines do you need?\n");
@@ -32,61 +55,63 @@ void main(void) {
 	cursor.ncols = ncolumns;
 	cursor.nrows = nrows;
 	cursor.player = 0;
-	cursor.marks[0] = 'x';
-	cursor.marks[1] = 'o';
+	strcpy(cursor.marks, "xo");
 
 	unsigned int ntiles = nrows * ncolumns;
 	char table[ntiles];
-	for (int i = 0; i < ntiles; i++) table[i] = ' ';
+	for (int i = 0; i < ntiles; i++) table[i] = 'x';
+	printf("Hello\n");
 
-	int ch;
-	int flag = 0;
-	unsigned int rounds = 0;
-	unsigned int table_index;
+	endwin();
+	//int ch;
+	//int flag = 0;
+	//unsigned int rounds = 0;
+	//unsigned int table_index;
 
-	while((ch = getch()) != KEY_F(1)) {
-		switch (ch) {
-			case KEY_UP:
-				cursor.y - 1 < 0 ? : cursor.y--; 
-				break;
-			case KEY_DOWN:
-				cursor.y + 1 >= nrows ? : cursor.y++;
-				break;
-			case KEY_LEFT:
-				cursor.x - 1 < 0 ? : cursor.x--;
-				break;
-			case KEY_RIGHT:
-				cursor.x + 1 >= ncolumns ? : cursor.x++;
-				break;
-			case KEY_BACKSPACE:
-				table_index = get_table_index(cursor.x, cursor.y, cursor.ncols);
+	//while((ch = getch()) != KEY_F(1)) {
+	//	switch (ch) {
+	//		case KEY_UP:
+	//			cursor.y - 1 < 0 ? : cursor.y--; 
+	//			break;
+	//		case KEY_DOWN:
+	//			cursor.y + 1 >= nrows ? : cursor.y++;
+	//			break;
+	//		case KEY_LEFT:
+	//			cursor.x - 1 < 0 ? : cursor.x--;
+	//			break;
+	//		case KEY_RIGHT:
+	//			cursor.x + 1 >= ncolumns ? : cursor.x++;
+	//			break;
+	//		case KEY_BACKSPACE:
+	//			table_index = get_table_index(cursor.x, cursor.y, cursor.ncols);
 
-				if (is_free(table, table_index)) {
-					attron(COLOR_PAIR(cursor.player + 1));
-					mvaddch(cursor.y, cursor.x, cursor.marks[cursor.player]);
-					table[table_index] = cursor.marks[cursor.player];
-					flag = is_winner(&cursor, table, marks_to_win);
-					if (flag) break;
-					cursor.player = !cursor.player;
-					rounds++;
-				}
-				break;
-		}
-		move(cursor.y, cursor.x);
-		refresh(); // refresh window
-		if (flag) {
-			break;
-		} else if (rounds == ntiles) {
-			break;
-		}
-	}
-	endwin(); // close window and end ncurses session
-	printf("Finished.\n");
-	if (flag) {
-		printf("Winner is %c!\n", cursor.marks[cursor.player]);
-	} else {
-		printf("Draw!\n", cursor.marks[cursor.player]);
-	}
+	//			if (is_free(table, table_index)) {
+	//				attron(COLOR_PAIR(cursor.player + 1));
+	//				mvaddch(cursor.y, cursor.x, cursor.marks[cursor.player]);
+	//				table[table_index] = cursor.marks[cursor.player];
+	//				flag = is_winner(&cursor, table, marks_to_win);
+	//				if (flag) break;
+	//				cursor.player = !cursor.player;
+	//				rounds++;
+	//			}
+	//			break;
+	//	}
+	//	move(cursor.y, cursor.x);
+	//	refresh(); // refresh window
+	//	if (flag) {
+	//		break;
+	//	} else if (rounds == ntiles) {
+	//		break;
+	//	}
+	//}
+	//endwin(); // close window and end ncurses session
+	//printf("Finished.\n");
+	//if (flag) {
+	//	printf("Winner is %c!\n", cursor.marks[cursor.player]);
+	//} else {
+	//	printf("Draw!\n", cursor.marks[cursor.player]);
+	//}
+	//save_to_file(&cursor, table);
 	//cursor.x = 0;
 	//cursor.y = 3;
 	//printf("x = %d, y = %d, i = %d\n", cursor.x, cursor.y, get_table_index(cursor.x, cursor.y, cursor.ncols));
