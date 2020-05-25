c56c6578418cd0e3597bc8a8be33f84e4c09a630
Mark Melczer <melczer7@gmail.com>
2020-05-25 21:25:19 +0200

    Figure out table pointer!

--- main.c
+++ main.c
@@ -7,23 +7,23 @@
 #include "gamedata.h"
 
 void main(void) {
-	unsigned int nrows = 5;
-	unsigned int ncolumns = 5;
-	unsigned int marks_to_win = 4;
+	//unsigned int nrows = 5;
+	//unsigned int ncolumns = 5;
+	//unsigned int marks_to_win = 4;
+	GameData gd = init_game_data();
 
 	Cursor cursor;
-	cursor.x = ncolumns / 2;
-	cursor.y = nrows / 2;
-	cursor.ncols = ncolumns;
-	cursor.nrows = nrows;
-	cursor.marks_to_win = marks_to_win;
-	cursor.player = 0;
-	strcpy(cursor.marks, "xo");
+	cursor.x = gd.cursor -> x;
+	cursor.y = gd.cursor -> y;
+	cursor.ncols = gd.cursor -> ncols;
+	cursor.nrows = gd.cursor -> nrows;
+	cursor.marks_to_win = gd.cursor -> marks_to_win;
+	cursor.player = gd.cursor -> player;
+	strcpy(cursor.marks, gd.cursor -> marks);
 
-	unsigned int ntiles = nrows * ncolumns;
-	char table[ntiles + 1];
-	for (int i = 0; i < ntiles; i++) table[i] = ' ';
-	table[ntiles] = '\0';
+	//char table[ntiles + 1];
+	//for (int i = 0; i < ntiles; i++) table[i] = ' ';
+	//table[ntiles] = '\0';
 
 	initscr(); // initialize curses
 	noecho(); // don't echo keystrokes (so it won't output what the user is typing)
@@ -35,13 +35,17 @@ void main(void) {
 
 	keypad(stdscr, TRUE); // enable special keys (such as F1 to exit)
 
-	WINDOW *win = newwin(nrows, ncolumns, 0, 0);
+	endwin();
+	printf("%s\n", gd.table);
+	/*
+	WINDOW *win = newwin(cursor.nrows, cursor.ncols, 0, 0);
 	//wborder(win, '*', '*', '*', '*', '*', '*', '*', '*');
 
 	int ch;
 	int flag = 0;
 	unsigned int rounds = 0;
 	unsigned int table_index;
+	unsigned int ntiles = cursor.nrows * cursor.ncols;
 
 	move(cursor.y, cursor.x); // move cursor to specific location
 	while((ch = getch()) != KEY_F(1)) {
@@ -50,22 +54,22 @@ void main(void) {
 				cursor.y - 1 < 0 ? : cursor.y--; 
 				break;
 			case KEY_DOWN:
-				cursor.y + 1 >= nrows ? : cursor.y++;
+				cursor.y + 1 >= cursor.nrows ? : cursor.y++;
 				break;
 			case KEY_LEFT:
 				cursor.x - 1 < 0 ? : cursor.x--;
 				break;
 			case KEY_RIGHT:
-				cursor.x + 1 >= ncolumns ? : cursor.x++;
+				cursor.x + 1 >= cursor.ncols ? : cursor.x++;
 				break;
 			case KEY_BACKSPACE:
 				table_index = get_table_index(cursor.x, cursor.y, cursor.ncols);
 
-				if (is_free(table, table_index)) {
+				if (is_free(gd.table, table_index)) {
 					attron(COLOR_PAIR(cursor.player + 1));
 					mvaddch(cursor.y, cursor.x, cursor.marks[cursor.player]);
-					table[table_index] = cursor.marks[cursor.player];
-					flag = is_winner(&cursor, table);
+					gd.table[table_index] = cursor.marks[cursor.player];
+					flag = is_winner(&cursor, gd.table);
 					if (flag) break;
 					cursor.player = !cursor.player;
 					rounds++;
@@ -87,21 +91,5 @@ void main(void) {
 	} else {
 		printf("Draw!\n", cursor.marks[cursor.player]);
 	}
-	//GameData gd_w = {&cursor, table};
-	//save_to_file(&gd_w);
-
-	//GameData gd_r = read_from_file();
-	//printf("%s\n", gd_r.table);
-	//cursor.x = 0;
-	//cursor.y = 3;
-	//printf("x = %d, y = %d, i = %d\n", cursor.x, cursor.y, get_table_index(cursor.x, cursor.y, cursor.ncols));
-	//table[0] = 'x';
-	//table[5] = 'o';
-	//table[10] = 'o';
-	//table[15] = 'o';
-	//for (int i = 0; i < tiles; i++) {
-	//	printf("t[%d] = %c\n", i, table[i]);
-	//}
-	//cursor.player = !cursor.player;
-	//printf("winner = %d\n", is_winner(&cursor, &table, marks_to_win));
+	*/
 }
